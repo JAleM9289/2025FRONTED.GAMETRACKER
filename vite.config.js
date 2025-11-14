331a5764d8c734577ab77f7ed1848f1a59ef8cc7
Alejandromite <jhonmite05@gmail.com>
2025-11-14 20:31:36 +0000

    comit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // AÑADE ESTA LÍNEA: debe ser el nombre exacto de tu repositorio.
-  base: "/2025FRONTED.GAMETRACKER/", 
+  // 1. CONFIGURACIÓN DE RUTA BASE PARA GITHUB PAGES
+  base: "/2025FRONTED.GAMETRACKER/",
+  
+  // 2. INYECCIÓN DE LA CLAVE RAWG EN EL CÓDIGO DE PRODUCCIÓN
+  // Esto soluciona el error 401 que ocurre cuando se ejecuta npm run deploy
+  define: {
+    'import.meta.env.VITE_RAWG_KEY': JSON.stringify('1d74913aa60c428c8a8f4bed0799fac4'),
+  },
 })
